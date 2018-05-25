52c31b508d2853a3f0f905ae82344bdd80613cb4
Negar-Ebrahimi <negarfreebots@gmail.com>
2018-05-25 06:45:59 +0430

    Fitness function works properly for every individual

--- Genetic.py
+++ Genetic.py
@@ -1,6 +1,7 @@
 from random import randint
 import math
 
+
 def resolutions_size(genotype):
     number_of_recessive_alleles = 0
     for allele in genotype:
@@ -8,19 +9,18 @@ def resolutions_size(genotype):
             number_of_recessive_alleles += 1
     # The number of resolutions will be half of the all possible parents ('cause they're coupled)
     if number_of_recessive_alleles == 0:
-        return 1
+        return 0
     return 2**(number_of_recessive_alleles - 1)
 
 
 def expand_resolution(genotype, resolution_number):
-    if resolution_number >= resolutions_size(genotype):  # Is this resolution_number available in the genotype
+    if resolution_number > resolutions_size(genotype):  # Is this resolution_number available in the genotype
         return False
     haplotype_mother = ()
     haplotype_father = ()
-    recessive_alleles = "{0:b}".format(resolution_number) + "0"
-    print('resolution number:', resolution_number)
-    print('resolution size:', resolutions_size(genotype))
-    print(("{0:b}".format(resolution_number)).zfill(int(math.log(resolutions_size(genotype), 2)) + 1))
+    recessive_alleles = ''
+    if resolutions_size(genotype) > 0:
+        recessive_alleles = ("{0:b}".format(resolution_number)).zfill(int(math.log(resolutions_size(genotype), 2)) + 1)
     recessive_allele_index = 0
     # Makes the j-th resolution couple of the genotype in which j = resolution_number
     for allele_index in range(len(genotype)):
@@ -34,23 +34,29 @@ def expand_resolution(genotype, resolution_number):
             recessive_allele_index += 1
     return [haplotype_mother, haplotype_father]
 
+
 def fitness(individual):
     haplotypes = ()
     for genotype_index in range(len(genotypes)):
-        haplotypes = haplotypes + (expand_resolution(genotypes[genotype_index], individual[genotype_index]),)
-    print(haplotypes)
-    print(len(set(haplotypes)))
-    return len(genotypes) - len(set(haplotypes))
+        haplotypes = haplotypes + (expand_resolution(genotypes[genotype_index], individual[genotype_index])[0],)  # Add mother
+        haplotypes = haplotypes + (expand_resolution(genotypes[genotype_index], individual[genotype_index])[1],)  # Add father
 
-def initial_population(genotypes):
-    initial_population = []
-    initial_population_size = 2
+    return 2*len(genotypes) - len(set(haplotypes))
+
+
+def initial_population():
+    population = []
+    initial_population_size = 2  # This can be changed
     for index in range(initial_population_size):
         individual = ()
         for genotype in genotypes:
-            individual = individual + (randint(0, resolutions_size(genotype) - 1),)
-        initial_population.append(individual)
-    return initial_population
+            if resolutions_size(genotype) > 0:
+                individual = individual + (randint(1, resolutions_size(genotype)) - 1,)
+            else:
+                individual = individual + (0,)  # This genotype has no recessive allele and has one identical resolution
+
+        population.append(individual)
+    return population
 
 
 # extracting the input data
@@ -60,5 +66,6 @@ with open("SmallSampleInput.txt") as file:
     for line in file:  # read rest of lines
         genotypes.append([int(x) for x in line.split()])
 
-    print(initial_population(genotypes))
-    print(fitness(initial_population(genotypes)[0]))
+    sample_individual = initial_population()[0]
+
+    print(fitness(sample_individual))
